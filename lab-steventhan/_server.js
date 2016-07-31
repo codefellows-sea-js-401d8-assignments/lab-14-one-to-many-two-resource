90ed003d445a73fc8b6353cf7c579a434541ec6e
Steven Than <steventhan11@gmail.com>
2016-07-31 07:00:12 -0700

    wrap up for the day

--- lab-steventhan/_server.js
+++ lab-steventhan/_server.js
@@ -12,13 +12,13 @@ server.get('/', (req, res) => {
   res.send('For user api, navigate to \'/api/users\'. For order api, navigate to \'/api/orders\'');
 });
 
+server.use(errorResponse);
+
 // Use userRouter at /api/users
 server.use('/api/users', userRouter);
 
 // Use orderRouter at /api/orders
 server.use('/api/orders', orderRouter);
-
-server.use(errorResponse);
 
 // 404 handling
 server.use((req, res, next) => {
--- lab-steventhan/routes/user_router.js
+++ lab-steventhan/routes/user_router.js
@@ -14,13 +14,39 @@ userRouter.get('/all', (req, res) => {
     .then(res.json.bind(res));
 });
 
+userRouter.get('/:id', (req, res, next) => {
+  let _id = req.params.id;
+  User.find({_id})
+    .then(res.json.bind(res))
+    .catch((error) => {
+      next(AppError.new404('Not Found', `Invalid user id from input. ${error.message}`));
+    });
+});
+
 userRouter.post('/', jsonParser, (req, res, next) => {
   let parsedJson = req.body;
   if (parsedJson.firstName && parsedJson.lastName && parsedJson.email) {
-    User(req.body).save()
-      .then(res.json.bind(res));
+    User(parsedJson).save()
+      .then(res.json.bind(res))
+      .catch((error) => {
+        next(AppError.new400('Bad Request', `${error.message}`));
+      });
   } else {
-    next(AppError.new400('Invalid JSON', 'Invalid JSON received from post request'));
+    next(AppError.new400('Invalid JSON', 'Invalid object properties received from posted JSON'));
+  }
+});
+
+userRouter.put('/:id', jsonParser, (req, res, next) => {
+  let parsedJson = req.body;
+  let _id = req.params.id;
+  if (parsedJson.firstName && parsedJson.lastName && parsedJson.email) {
+    User.findOneAndUpdate({_id}, parsedJson)
+      .then(res.json.bind(res))
+      .catch((error) => {
+        next(AppError.new404('Not Found', `Invalid user id from input. ${error.message}`));
+      });
+  } else {
+    next(AppError.new400('Invalid JSON', 'Invalid object properties received from put JSON'));
   }
 });
 
