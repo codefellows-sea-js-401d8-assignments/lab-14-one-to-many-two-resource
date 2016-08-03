47739689080bea708b2dc4f7d75bba1f8471d445
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-02 22:06:18 -0700

    added to server

--- lab-aliza/server.js
+++ lab-aliza/server.js
@@ -3,7 +3,12 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const router = require(__dirname + '/route/router.js');
+const pandaRouter = require(__dirname + '/routes/pandarouter.js');
+const partyRouter = require(__dirname + '/routes/partyrouter.js');
+const Promise = require('./lib/promise');
+mongoose.Promise = Promise;
+const debug = require('debug');
+const serverError = debug('serverError');
 
 const LOCAL_DB_SERVER = 'mongodb://localhost/dev_db';
 const DB_SERVER = process.env.DB_SERVER || LOCAL_DB_SERVER;
@@ -11,10 +16,15 @@ const port = 3000;
 
 mongoose.connect(DB_SERVER);
 
-app.use('/api', router);
+app.use('/api/panda', pandaRouter);
+app.use('/api/party', partyRouter);
 
-app.listen(port, () => {
-  console.log('app listening on port ' + port);
+app.use((err, req, res, next) => {
+  serverError(err);
+  res.status(err.statusCode).json(err.message);
+  next();
 });
+
+app.listen(port, () => console.log('server up'));
 
 module.exports = app;
